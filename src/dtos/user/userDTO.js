88ff9465e90f0cab8072c54f14c71817cd737875
Martin <marteeeen@gmail.com>
2023-07-23 23:17:25 -0300

    tercera

--- src/dtos/user/userDTO.js
+++ src/dtos/user/userDTO.js
@@ -1,10 +1,9 @@
 export default class UserDTO {
-    constructor(user) {
-      (this.name = `${user.firstName} ${user.lastName}`),
-        (this.role = user.role),
-        (this.id = user._id),
-        (this.email = user.email),
-        (this.cart = user.cart._id);
-    }
+  constructor(user) {
+    (this.name = user.first_name),
+      (this.role = user.role),
+      (this.id = user._id),
+      (this.email = user.email),
+      (this.cart = user.cart._id);
   }
-  
+}
